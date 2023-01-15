40329a5beec9a530d44905d8cb28e5291990471c
AleksandrM <mutmaya@gmail.com>
2023-01-15 22:18:09 +0300

    Add fldr Lesson_1 & 1-2 file

---
+++ Lesson_1/2.py
@@ -0,0 +1,29 @@
+hh = '00'
+mm = '00'
+sec = int(input('Введите время в секундах: '))
+
+if sec < 60:
+     if sec < 10:
+        print(hh, ':', mm, ':', '0' + str(sec))
+     else:
+        print(hh, ':', mm, ':', sec)
+elif 3600 > sec > 60:
+    mm = sec // 60
+    sec = sec % 60
+    if mm < 10:
+        print(hh, ':', '0' + str(mm), ':', sec)
+    else:
+        print(hh, ':', mm, ':', sec)
+elif sec >= 3600:
+    hh = sec // 3600
+    sec = sec - (hh * 3600)
+    if sec < 60:
+         print(hh, ':', mm, ':', sec)
+    elif 3600 >= sec > 60:
+        mm = sec // 60
+        sec = sec % 60
+        if mm < 10:
+            print(hh, ':', '0' + str(mm), ':', sec)
+        else:
+            print(hh, ':', mm, ':', sec)
+    print(hh, ':', mm, ':', sec)
